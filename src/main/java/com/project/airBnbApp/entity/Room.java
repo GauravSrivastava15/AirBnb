f09a5afe81565e501ce85da805d60bffddd371f9
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-09-19 00:00:30 +0530

    Entity classes setup completed

--- src/main/java/com/project/airBnbApp/entity/Room.java
+++ src/main/java/com/project/airBnbApp/entity/Room.java
@@ -18,7 +18,7 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "hotel_id", nullable = false)
     private Hotel hotel;
 
---
+++ src/main/java/com/project/airBnbApp/entity/User.java
@@ -0,0 +1,31 @@
+package com.project.airBnbApp.entity;
+
+import com.project.airBnbApp.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user")  // by default postgress uses user table internally so we need to update the name, we cannot have another user table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
