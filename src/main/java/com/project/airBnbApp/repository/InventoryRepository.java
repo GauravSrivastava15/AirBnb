e89c6f3c6d1e1e03b41e764513089761499152c6
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-12-27 10:22:40 +0530

    Implemented the inventory search api with custom query

--- src/main/java/com/project/airBnbApp/repository/InventoryRepository.java
+++ src/main/java/com/project/airBnbApp/repository/InventoryRepository.java
@@ -1,8 +1,13 @@
 package com.project.airBnbApp.repository;
 
+import com.project.airBnbApp.entity.Hotel;
 import com.project.airBnbApp.entity.Inventory;
 import com.project.airBnbApp.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,4 +15,23 @@ import java.time.LocalDate;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     void deleteByRoom(Room room);
+
+    @Query("""
+            Select distinct i.hotel
+            from Inventory i
+            where i.city = :city
+                and i.date between :startDate and :endDate
+                and i.closed = false
+                and (i.totalCount - i.bookedCount) >= :roomsCount
+            Group by i.hotel, i.room
+            Having Count(i.date) = :dateCount
+            """)
+    Page<Hotel> findHotelWithAvailableInventory(
+            @Param("city") String city,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("roomsCount") Integer roomsCount,
+            @Param("dateCount") Long dateCount,
+            Pageable pageable
+    );
 }
--- src/main/java/com/project/airBnbApp/service/InventoryServiceImpl.java
+++ src/main/java/com/project/airBnbApp/service/InventoryServiceImpl.java
@@ -1,20 +1,30 @@
 package com.project.airBnbApp.service;
 
+import com.project.airBnbApp.dto.HotelDto;
+import com.project.airBnbApp.dto.HotelSearchRequest;
+import com.project.airBnbApp.entity.Hotel;
 import com.project.airBnbApp.entity.Inventory;
 import com.project.airBnbApp.entity.Room;
 import com.project.airBnbApp.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class InventoryServiceImpl implements InventoryService {
     final private InventoryRepository inventoryRepository;
+    final private ModelMapper modelMapper;
 
     @Override
     public void initializeRoomForAYear(Room room) {
@@ -39,5 +49,18 @@ public class InventoryServiceImpl implements InventoryService {
     @Override
     public void deleteAllInventories(Room room) {
         inventoryRepository.deleteByRoom(room);
+    }
+
+    @Override
+    public Page<HotelDto> searchHotels(HotelSearchRequest hotelSearchRequest) {
+        long dateCount =
+                ChronoUnit.DAYS.between(hotelSearchRequest.getStartDate(), hotelSearchRequest.getEndDate()) + 1;
+
+        Pageable pageable = PageRequest.of(hotelSearchRequest.getPage(), hotelSearchRequest.getSize());
+        Page<Hotel> hotelPage = inventoryRepository.findHotelWithAvailableInventory(hotelSearchRequest.getCity(),
+                hotelSearchRequest.getStartDate(), hotelSearchRequest.getEndDate(), hotelSearchRequest.getRoomsCount(),
+                dateCount, pageable);
+
+        return hotelPage.map((element) -> modelMapper.map(element, HotelDto.class));
     }
 }
